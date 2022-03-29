64f762857a7c7ee4510dde629acb022bf04127e0
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2022-03-28 22:06:50 -0400

    finished out for the day. Will do at least 20 commits a day from now on.

---
+++ assets/admin/projectProgress.js
@@ -0,0 +1,58 @@
+const projectProgress = {
+
+   Admin: {
+      "This file was created so that I can just leave notes to myself as I build. This is something that can definitely help us as we go down our developer road. Write here as time passeth."
+
+      PortfolioDesired_FinishDate: ["Tue", '03-28-22'],
+      PortfolioActual_FinishDate: ["", ''],
+   }
+
+   logTemplate: {
+
+      Entry00: {
+         Header: {
+            Level: ["Goal", 'Objective', 'task'],
+            Title: "",
+            TimeExpectedStart: '',
+            TimeExpectedFinish: '',
+            TimeActualStart: '',
+            TimeActualFinish: '',
+         },
+   
+         Data: {
+            Chapter00: [
+               "",
+               '',
+            ],
+         },
+      },
+
+   },
+
+
+
+   // LogBook
+
+   PortfolioLogBook: {
+      Entry00: {
+         Header: {
+            Level: ["Goal", 'Objective', 'task'],
+            Title: "",
+            TimeExpectedStart: '',
+            TimeExpectedFinish: '',
+            TimeActualStart: '',
+            TimeActualFinish: '',
+         },
+   
+         Data: {
+            Chapter00: [
+               "Important Notes for 3-28-22",
+               'Decide how we are going to use the GIFs in our project, and consider doing a looped video instead of a GIF if the gif files do not work in the image files. Find alternatives for the css renderer and use as few as them as possible. Save those only for buttons. As they dont seem to take up alot of GL.',
+            ],
+         },
+      },
+   },
+
+
+
+}
